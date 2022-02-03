37d5413fd550fd61174a53bcff3e7fc9109028f6
kazy <>
2022-02-03 15:49:20 +0900

    バニラでDOM操作

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import "./styles.css";
+console.log("test");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const title = document.getElementById("title");
+console.log(title);
+
+const title2 = document.querySelector("#title");
+console.log(title2);
+
+const containers = document.getElementsByClassName("container");
+console.log(containers);
+
+const divEl = document.createElement("div");
+const pEl = document.createElement("p");
+const h2El = document.createElement("h2");
+divEl.appendChild(pEl);
+divEl.prepend(h2El);
+
+console.log(divEl);
+
+const buttonEl = document.createElement("button");
+buttonEl.textContent = "ボタン";
+const div1El = document.querySelector(".container");
+div1El.appendChild(buttonEl);
+
+const h1El = document.getElementById("title");
+const bodyEl = document.querySelector("body");
+bodyEl.removeChild(h1El);
