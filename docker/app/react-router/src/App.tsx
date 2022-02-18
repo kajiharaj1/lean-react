51ab2cb89e436685679d95f56cd712f215ecbc1a
kajihara.junichi <71058234+kajiharaj1@users.noreply.github.com>
2022-02-18 15:20:10 +0900

    Routeの切り出し

--- docker/app/react-router/src/App.tsx
+++ docker/app/react-router/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Link, Routes, Route, Outlet } from "react-router-dom";
 
-import { Home } from './Home'
-import { Page1 } from './Page1'
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
-import { Page2 } from './Page2'
+import { Router } from './router/Router'
 
 function App() {
   return (
@@ -17,14 +13,7 @@ function App() {
         <br />
         <Link to="/page2">page2</Link>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page1/detailA" element={<Page1DetailA />} />
-        <Route path="/page1" element={<Page1 />} />
-          <Route path="/page1/detailA" element={<Page1DetailA />} />
-          <Route path="/page1/detailB" element={<Page1DetailB />} />
-        <Route path="/page2" element={<Page2 />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   );
 }
