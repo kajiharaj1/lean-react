da42cf3cd170749d4e474b538af4e1b70bf39149
kajihara.junichi <71058234+kajiharaj1@users.noreply.github.com>
2022-02-18 15:05:26 +0900

    ネストされたページ遷移

--- docker/app/react-router/src/App.tsx
+++ docker/app/react-router/src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Routes, Route, Outlet } from "react-router-dom";
 
 import { Home } from './Home'
 import { Page1 } from './Page1'
+import { Page1DetailA } from './Page1DetailA';
+import { Page1DetailB } from './Page1DetailB';
 import { Page2 } from './Page2'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Home />
-        <Page1 />
-        <Page2 />
+        <Link to="/">home</Link>
+        <br />
+        <Link to="/page1">page1</Link>
+        <br />
+        <Link to="/page2">page2</Link>
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1/detailA" element={<Page1DetailA />} />
+        <Route path="/page1" element={<Page1 />} />
+          <Route path="/page1/detailA" element={<Page1DetailA />} />
+          <Route path="/page1/detailB" element={<Page1DetailB />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
