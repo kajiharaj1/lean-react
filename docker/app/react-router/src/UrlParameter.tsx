231ce24468f4fecb82dfc43195531ab2368b0e97
kajihara.junichi <71058234+kajiharaj1@users.noreply.github.com>
2022-02-18 15:48:06 +0900

    クエリパラメータの取得

--- docker/app/react-router/src/UrlParameter.tsx
+++ docker/app/react-router/src/UrlParameter.tsx
@@ -2,13 +2,15 @@ import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { id } = useParams();
-    const location = useLocation();
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
+    console.log(query);
 
-    console.log(location);
     return (
         <div>
-            <p>パラメータは {id} です</p>
             <h1>UrlParameterページです</h1>
+            <p>パラメータは {id} です</p>
+            <p>クエリパラメータは {query.get("name")} です</p>
         </div>
     )
 }
