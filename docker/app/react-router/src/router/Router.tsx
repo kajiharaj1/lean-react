776fba4d14a697c660b902c329c95a2cb790d3b1
kajihara.junichi <71058234+kajiharaj1@users.noreply.github.com>
2022-02-18 16:24:18 +0900

    404ページ

--- docker/app/react-router/src/router/Router.tsx
+++ docker/app/react-router/src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Page1 } from "../Page1"
 import { Page1DetailA } from "../Page1DetailA"
 import { Page1DetailB } from "../Page1DetailB"
 import { Page2 } from "../Page2"
+import { Page404 } from "../Page404"
 import { UrlParameter } from "../UrlParameter"
 
 export const Router = () => {
@@ -17,6 +18,7 @@ export const Router = () => {
         <Route path="/page2" element={<Page2 />}>
             <Route path=":id" element={<UrlParameter />} />
         </Route>
+        <Route path="*" element={<Page404 />} />
       </Routes>
     )
 }
