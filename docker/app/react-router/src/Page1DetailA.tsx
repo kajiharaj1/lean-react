181c56589902357f7e7ddb05147f8d316ed30bfd
kajihara.junichi <71058234+kajiharaj1@users.noreply.github.com>
2022-02-18 16:20:47 +0900

    Linkを使わないページ遷移

--- docker/app/react-router/src/Page1DetailA.tsx
+++ docker/app/react-router/src/Page1DetailA.tsx
@@ -1,11 +1,16 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export const Page1DetailA = () => {
     const { state } = useLocation();
     console.log(state);
+
+    const onClickBack = () => navigate(-1);
+
+    const navigate = useNavigate();
     return (
         <div>
             <h1>Page1DetailAページです</h1>
+            <button onClick={onClickBack}>戻る</button>
         </div>
     )
 }
